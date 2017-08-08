cc63a9975cb085cc5ae8799fbd5b857f7c6a893a
amey <Amay>
2017-08-08 13:55:04 +0530

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -49,18 +49,13 @@
             <div class="sidebar-wrapper">
                 <ul class="nav">
                     <li class="active">
-                        <a href="dashboard.html">
+                        <a href="/home">
                             <i class="material-icons">dashboard</i>
                             <p>Dashboard</p>
                         </a>
                     </li>
              
-                    <li>
-                        <a href="table.html">
-                            <i class="material-icons">content_paste</i>
-                            <p>Table List</p>
-                        </a>
-                    </li>
+              
         
                 </ul>
             </div>
